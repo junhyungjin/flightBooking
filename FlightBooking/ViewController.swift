3d5b7c3c6d5519958d6b12b78183f1e9654e4eab
Hyung jin Jun <junhyungjin@me.com>
2016-05-12 23:46:49 +0900

    flight booking #5

--- FlightBooking/ViewController.swift
+++ FlightBooking/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var departureDateButton: UIButton!
+    @IBOutlet var returnDateLabel: UILabel!
+    @IBOutlet var returnDateButton: UIButton!
+    @IBOutlet var selectDatePicker: UIDatePicker!
+    
+    @IBAction func showDatePickerAction(sender: AnyObject) {
+        print(sender.description!)
+    }
+    
+    
+    @IBAction func showReturnDateAction(sender: AnyObject) {
+     //   if ( sender as! UISwitch).on {
+     //       returnDateLabel.hidden = false
+     //       returnDateButton.hidden = false
+     //   } else {
+     //       returnDateLabel.hidden = true
+     //      returnDateButton.hidden = true
+     //   }
+        
+        returnDateLabel.hidden = !(sender as! UISwitch).on
+        returnDateButton.hidden = !(sender as! UISwitch).on
+    }
+    
+    @IBAction func selectedDateAction(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        returnDateLabel.hidden = true
+        returnDateButton.hidden = true
+        selectDatePicker.hidden = true
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        selectDatePicker.hidden = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
